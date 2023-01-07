65d6382ddb943ec12bb2b2e8e1b2bcb8f517500c
abdulkareemm <eng.kreem.96@gmail.com>
2023-01-07 10:46:42 -0800

    footer components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
+
+// logo
+import LogoWhite from "../assets/img/logo-white.svg";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-primary py-12">
+      <div className="container mx-auto text-white flex justify-between ">
+        {/* logo */}
+        <a href="">
+          <img src={LogoWhite} alt="" />
+        </a>
+        Copyright &copy; 2022. All rights reserved.
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
